f3379927f29c539364d858821326996cb24bedbd
tank11110 <56151436+tank11110@users.noreply.github.com>
2019-12-06 21:20:47 +0800

    Update HashTable.py

--- 作業四/HashTable.py
+++ 作業四/HashTable.py
@@ -1,1 +1,36 @@
+class  ListNode:
+    def __init__(self,val):
+        self.val=val
+        self.next=None
+    def setnext(self,node):
+        self.next=node
+    def getnext(self):
+        return self.next
 
+class MyHashSet:
+    def __init__(self,capacity=5):
+        self.capacity=capacity
+        self.data=[None]*capacity
+    def hashf(self,key):
+        import hashlib
+        key=hashlib.md5()                                                                                                                                                            
+        key.update(key)
+        hashkey=key.hexdigest()
+        return hashkey
+    def add(self,key):
+        hashkey=self.hashf(key)
+        hashval=[key]
+        self.data[hashkey]=[keyval]
+        return True
+    def contains(self,key):
+        hashkey=self.hashf(key)
+        if self.data[hashkey] is not None:
+            print("True")
+        else:
+            print("False")
+    def delete(self,key):
+        hashkey=self.hashf(key)
+        if self.data[hashkey] is None:
+            print("doesn't exist")
+        else:
+            self.data[hashkey].pop()
