ebf19d09c2c70417bc95544492266a448b2cbcaa
tank11110 <56151436+tank11110@users.noreply.github.com>
2019-12-06 23:57:46 +0800

    Update HashTable.py

--- 作業四/HashTable.py
+++ 作業四/HashTable.py
@@ -11,30 +11,33 @@ class  ListNode:  #連結點的設置
     def getnext(self):
         return self.next
 
-class MyHashSet:  #主程式
+class MyHashSet:
     def __init__(self,capacity=5):
         self.capacity=capacity
         self.data=[None]*capacity
-    def hashf(self,key):  #def一個MD5 function方便得到MD5值
+    def hashf(self,key):
         import hashlib
-        key=hashlib.md5()                                                                                                                                                            
-        key.update(key)
-        hashkey=key.hexdigest()
+        hkey=hashlib.md5()                                                                                                                                                            
+        hkey.update(key.encode("utf-8"))
+        hashkey=hkey.hexdigest()
         return hashkey
-    def add(self,key):  #在list中增加num的MD5-hash值
-        hashkey=self.hashf(key)  #呼叫hashf得到MD5值
-        hashval=[key]  #設置hashval儲存num
-        self.data[hashkey]=[keyval]   #在data中做出一個MD5-hash的值的位置並儲存num
-        return True
-    def contains(self,key):  #尋找num是否存在
-        hashkey=self.hashf(key)   #設置hashkey儲存hash-MD5的值
-        if self.data[hashkey] is not None:  #在data(list)中如果hashkey的位置不是空的
-            print("True")  #代表存在，並印出 True
-        else:
-            print("False")  #代表不存在，並印出 False
-    def delete(self,key):   #在lisr中刪除num的MD5-hash值
-        hashkey=self.hashf(key)  #設置hashkey儲存hash-MD5的值
-        if self.data[hashkey] is None:  #在data(list)中如果hashkey的位置是空的
-            print("doesn't exist")  #表示不存在，並印出"不存在"
-        else:   
-            self.data[hashkey].pop()   #表示存在，並把數列的值刪除
+    def add(self,key):
+        hashkey=self.hashf(key)
+        for i in range(0,5):
+            if self.data[i] is None:
+                self.data[i]=hashkey
+                break
+    def contains(self,key):
+        count=0
+        hashkey=self.hashf(key)
+        for i in range(0,5):
+            if self.data[i]==hashkey:
+                return True
+                break          
+            else:
+                return False
+    def delete(self,key):
+        hashkey=self.hashf(key)
+        for i in range(0,5):
+            if self.data[i]==hashkey:
+                self.data[i].pop()
