11ec9bf38b6ef77db28d541ddfd2a04bd7493994
tank11110 <56151436+tank11110@users.noreply.github.com>
2022-01-01 17:22:22 +0800

    Update finish.php

--- PHP與MYSQL/購物網站實作/第一版購物網站/PHP檔案/finish.php
+++ PHP與MYSQL/購物網站實作/第一版購物網站/PHP檔案/finish.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 session_start(); //開啟session
-$result = $link -> query("INSERT order (name,goods_id,goods_name,goods_price,goods_num) 
+$result = $link -> query("INSERT user_order(name,goods_id,goods_name,goods_price,goods_num) 
 SELECT name,goods_id,goods_name,goods_price,goods_num
 FROM shop_cart");
 echo "購買完成";
