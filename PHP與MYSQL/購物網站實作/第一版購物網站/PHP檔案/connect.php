b73e5fe46ecc0aaeea5faf402dc370d650ab5b2c
tank11110 <56151436+tank11110@users.noreply.github.com>
2023-12-27 23:47:09 +0800

    Update connect.php

--- PHP與MYSQL/購物網站實作/第一版購物網站/PHP檔案/connect.php
+++ PHP與MYSQL/購物網站實作/第一版購物網站/PHP檔案/connect.php
@@ -1,6 +1,6 @@
 <?php
     // 設定 MySQL 的連線資訊並開啟連線
     // 資料庫位置、使用者名稱、使用者密碼、資料庫名稱
-    $link = mysqli_connect("localhost", "root", "*********", "test");
+    $link = mysqli_connect("localhost", "root", "************", "test");
     $link -> set_charset("UTF8"); // 設定語系避免亂碼
 ?>
