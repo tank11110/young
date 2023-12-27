7d9c315ef4b42857dfd66b5b43408d0c38258909
tank11110 <56151436+tank11110@users.noreply.github.com>
2023-12-27 11:32:48 +0800

    Add files via upload

---
+++ PHP與MYSQL/購物網站實作/新版購物網站/管理者端/shop_type.php
@@ -0,0 +1,29 @@
+<?php
+	$choose_type="";
+	if(isset($choose_type))
+	{
+		$choose_type=$_POST["choose_type"];
+		switch ($choose_type) 
+		{
+			case '全部':
+				$type="全部";
+				header("location:shop.php?type=$type");
+				break;
+			
+			case '漢堡':
+				$type="漢堡";
+				header("location:shop.php?type=$type");
+				break;
+
+			case '炸物':
+				$type="炸物";
+				header("location:shop.php?type=$type");
+				break;
+
+			case '飲料':
+				$type="飲料";
+				header("location:shop.php?type=$type");
+				break;
+		}
+	}
+?>
