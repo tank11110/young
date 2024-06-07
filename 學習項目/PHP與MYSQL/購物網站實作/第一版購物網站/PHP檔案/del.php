de87d89662f638ab4559e56e3029cf8ff5b832a0
young <young94104@yahoo.com.tw>
2024-06-07 13:47:39 +0800

    修改ignore項目

--- 學習項目/PHP與MYSQL/購物網站實作/第一版購物網站/PHP檔案/del.php
+++
@@ -1,9 +0,0 @@
-<input type ="button" onclick="history.back()" value="回到購物車">
-<?php
-include 'connect.php';
-$GET_id=$_GET["id"]; //透過session讀取要刪除的ID
-$sql = $link -> query("DELETE FROM `shop_cart` WHERE id ='$GET_id'");
-//刪除指定ID的資料列
-echo $GET_id;
-header("location:buy.php");  //回購物車頁面
-?>
--- 學習項目/自然語言/jieba/jieba_06170135.py
+++
@@ -1,7 +0,0 @@
-import jieba
-import jieba.posseg as pseg
-sen='楓之谷伺服器每週例行維護時間是什麼時候呢?'
-print("輸入："+sen)
-words = jieba.cut(sen, cut_all=False)
-print("分詞結果：")
-print(' '.join(words))
